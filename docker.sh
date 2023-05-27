e8dec53f943bb23ff567f2477291134ad95213a2
Kirill Gubanov <gubanof.kiril@yandex.ru>
2023-05-27 15:58:39 +0300

    updated docker.sh

--- docker.sh
+++ docker.sh
@@ -1,9 +1,9 @@
 apt update
-apt install apt-transport-https
+apt install apt-transport-https -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
-apt install docker-ce docker-compose
+apt install docker-ce docker-compose -y
 usermod -aG docker user
 mv ./micro.service /etc/systemd/system/
 systemctl daemon-reload
